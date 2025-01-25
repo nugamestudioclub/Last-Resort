3aaca07f683e8ce193c953becb275f9301fc1aed
juan-carlos <carlosalberto.j@northeastern.edu>
2025-01-24 20:00:19 -0500

    basic movement and mouse camera controls added

--- player-movement.gd
+++ player-movement.gd
@@ -1,26 +1,21 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 10.0
 
 
 func _physics_process(delta: float) -> void:
-	# will be replaced for 2d top down movement
-	# Add the gravity.
-	#if not is_on_floor():
-	#	velocity += get_gravity() * delta
-
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	#	velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	#var direction := Input.get_axis("ui_left", "ui_right")
-	#if direction:
-	#	velocity.x = direction * SPEED
-	#else:
-	#	velocity.x = move_toward(velocity.x, 0, SPEED)
+	var direction: Vector2
+	direction.x = Input.get_axis("ui_left", "ui_right")
+	
+	direction.y = Input.get_axis("ui_up", "ui_down")
+	
+	#translate(direction * SPEED)
+	move_and_collide(direction * SPEED)
 
-	move_and_slide()
+# camera movement
+func _process(delta: float) -> void:
+	look_at(get_global_mouse_position())
